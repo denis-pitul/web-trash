e6e761473574670866f7856783487532ee4ef8af
Denis Pitul <denis.pitul@gmail.com>
2013-12-17 00:22:40 +0200

    and now it's alright, we shall call this v0.1a

--- log/urls.py
+++ log/urls.py
@@ -1,4 +1,9 @@
 from django.conf.urls import patterns, url
 from log import views
 
-urlpatterns = patterns('', url(r'^$', views.index, name='index'))
+urlpatterns = patterns('', 
+        url(r'^$', views.index, name='index'),
+        url(r'^new$', views.new, name='new'),
+        url(r'^add$', views.add, name='add'),
+        url(r'^view$', views.view, name='view'),
+)
--- log/views.py
+++ log/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext, loader, Template, Context
+from django.core.urlresolvers import reverse
 
 from log.models import Entry
 
-import time
+from datetime import datetime
 
 # Create your views here.
 
@@ -12,12 +13,28 @@ def index(request):
     entries = Entry.objects.order_by('-date')
     template = loader.get_template('log/index.html')
     context = RequestContext(request, {
-        'entries': entries,
+        'entries_list': entries,
     })
     return HttpResponse(template.render(context))
 
+def view(request):
+    entry_id = int(request.GET['entry_id'])
+    entries = Entry.objects.order_by('-id')
+    found_entry = None
+    for entry in entries:
+        if (entry.id == entry_id):
+            found_entry = entry
+    if (found_entry == None):
+        return HttpResponse('Entry not found!')
+    return HttpResponse(Template('{{ text|linebreaksbr }}').render(Context({'text': found_entry.text})))
+
+def new(request):
+    template = loader.get_template('log/new.html')
+    return render(request, 'log/new.html', { })
+
 def add(request):
     entry = Entry()
-    entry.date = time
-    entry.text = None
-    return HttpResponse('Under construction')
+    entry.date = datetime.now()
+    entry.text = request.POST['text']
+    entry.save()
+    return HttpResponseRedirect(reverse('index'))
