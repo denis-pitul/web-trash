6e069d7120addefef32a8e72bf03730d504b6c49
Denis Pitul <denis.pitul@gmail.com>
2013-12-16 22:57:21 +0200

    working

--- log/views.py
+++ log/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from log.models import Entry
+
+import time
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello")
+    entries = Entry.objects.order_by('-date')
+    template = loader.get_template('log/index.html')
+    context = RequestContext(request, {
+        'entries': entries,
+    })
+    return HttpResponse(template.render(context))
+
+def add(request):
+    entry = Entry()
+    entry.date = time
+    entry.text = None
+    return HttpResponse('Under construction')
